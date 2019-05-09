38e22283782225985c11714abc6a657f2ae6dffc
Tomasz Fejcak <t.fejcak@AMDC3740.digital.local>
2019-05-09 13:13:19 +0200

    recursive solution working properly for any size of input

--- tv_shows.py
+++ tv_shows.py
@@ -1,80 +1,77 @@
 # add your own imports and auxiliary functions if needed
 import numpy as np
 import pprint
-
-
+import random
+import time
+dct = {}
 def get_max_product(a, b,a_idx = 0, b_idx = 0):
-  r1 = r2 = r3 = 0
-
   if a_idx == b_idx == len(a):
     return 0
+  v = dct.get((a_idx, b_idx), None)
+  if v is not None:
+    return v
+  r1 = r2 = r3 = 0
 
   if a_idx + 2 <= len(a):
     r1 = a[a_idx]*a[a_idx+1] + get_max_product(a,b,
-       a_idx= a_idx + 2,
+      a_idx= a_idx + 2,
         b_idx= b_idx)
 
   if a_idx + 1 <= len(a) and b_idx + 1 <= len(b):
     r2 = a[a_idx]*b[b_idx] + get_max_product(a,b,
       a_idx = a_idx+1,
-        b_idx = b_idx+1)
+        b_idx = b_idx+1) 
 
   if b_idx + 2 <= len(a):
     r3 = b[b_idx]*b[b_idx+1] + get_max_product(a,b,
-       a_idx= a_idx,
+      a_idx= a_idx,
         b_idx= b_idx + 2)
 
-  return max(r1,r2,r3)
+  val = max(r1,r2,r3)
+  dct[(a_idx, b_idx)] = val
+  return val  
 
 def get_max_product_dp(a,b):
-  
-  matrix = np.zeros(shape=(len(a),len(a)))
-  a_idx = 0
-  b_idx = 0
 
-  while a_idx <= len(a):
-    while b_idx <= len(b):
-      r1 = r2 = r3 = 0
-      if a_idx + 2 <= len(a):
-        r1 = a[a_idx]*a[a_idx-1] + matrix[a_idx-2,b_idx]
-
-      if a_idx + 1 <= len(a) and b_idx + 1 <= len(b):
-        r2 = a[a_idx]*b[b_idx] + matrix[a_idx-1, b_idx-1]
-
-      if b_idx + 2 <= len(a):
-        r3 = b[b_idx]*b[b_idx-1] + matrix[a_idx,b_idx-2]
-
-      maximum = max(r1,r2,r3)
-      matrix[a_idx,b_idx] = maximum
-      if r1 == maximum: 
-        a_idx+=2
-      elif r2 == maximum: 
-        a_idx += 1 
-        b_idx += 1
-      else : 
-        b_idx += 2
-
-      print(matrix)
-    return matrix.max
+  result = np.zeros(shape=(len(a)+1,len(a)+1))
+  for i in range(0,result.shape[0]):
+    for j in range(0, result.shape[1]):
+      if i == 0 and j == 0:
+        result[i,j] = 0
+      elif i == 1 and j == 0:
+        result[i, j] = a[i-1]
+      elif j == 1 and i == 0:
+        result[i,j] = b[j-1]
+      elif i == j == 1:
+        result[i,j] = a[i-1]*b[j-1]
+      else :
+        result[i,j] = max (
+          result[i-1,j-1] + a[i-1]*b[j-1],
+          result[i-2,j] + a[i-2]*a[i-1],
+          result[i,j-2] + b[j-2]*b[j-1]
+        )
+  print(result)
+  return result.max
   
 def tvshows(a, b):
   # implement your solution here
-  t = get_max_product_dp(a,b)
+  # t = get_max_product_dp(a,b)
   return get_max_product(a,b)
 
-# test_a = [53,20,50,22,63,43,43,39,83,76]
-# test_b = [73,83,10,23,34,24,0,77,33,32]
+test_a = [random.randint(0,100) for i in range(0,500)]
+test_b = [random.randint(0,100) for i in range(0,500)]
 
 
 
 
-test_a = [1,2]
-test_b = [3,4]
+# test_a = [1,2]
+# test_b = [3,4]
 
 expected_result = 23066
-
+start = time.time()
 my_result = tvshows(test_a, test_b)
-print(my_result)
+end = time.time()
+print(f"Result equal to {my_result} obtained in {end-start} seconds")
 
 if my_result == expected_result:
   print("Result correct!")
